132de1aa4307b672fd66c9aa31de85421364d80a
siddikur-dev <siddikur.dev@gmail.com>
2025-08-28 11:31:07 +0600

    alert function added'

--- js/index.js
+++ js/index.js
@@ -7,6 +7,7 @@ function strToNumber(id) {
 
 // Heart count Function
 const heartIcons = document.getElementsByClassName("lucide-heart");
+
 for (const heartIcon of heartIcons) {
   heartIcon.addEventListener("click", function () {
     //get heart from navbar
@@ -58,3 +59,42 @@ function setupCopyButtons() {
 }
 
 setupCopyButtons();
+
+//Calling Function
+
+//get all id from
+function getInnerText(id) {
+  const allId = document.getElementById(id).innerText;
+  return allId;
+}
+
+// get all call button by className
+
+const allCallingBtn = document.getElementsByClassName("callBtn");
+
+for (const callBtn of allCallingBtn) {
+  callBtn.addEventListener("click", function () {
+    //get coin from navbar
+    const availableCoin = document.getElementById("availableCoin");
+    let currentCoin = strToNumber("availableCoin");
+    const remainingCoin = currentCoin - 20;
+    availableCoin.innerText = remainingCoin;
+  });
+}
+
+//alert function
+
+const allCallingBtns = document.getElementsByClassName("callBtn");
+
+for (const callBtn of allCallingBtns) {
+  callBtn.addEventListener("click", function () {
+    const card = this.closest(".card");
+
+    // card এর ভেতরের helplines এর number না
+    const helpline = card.querySelector(".helplines").innerText;
+    const cardTitle = card.querySelector(".cardTitleText").innerText;
+
+    // alert দেখাও
+    alert(`📞 Calling ${cardTitle} ${helpline}...`);
+  });
+}
